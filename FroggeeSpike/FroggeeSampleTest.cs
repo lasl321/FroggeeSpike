c08a498a736e291c60bc42a807886f5fbfd0ed9a
Luis Sotomayor <luis.sotomayor@gmail.com>
2015-01-01 19:10:55 -0800

    Initial commit (Chrome only).

---
+++ FroggeeSpike/FroggeeSampleTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Froggee.Framework;
+using OpenQA.Selenium;
+
+namespace Froggee.Framework.Sample
+{
+    public class FroggeeSampleTest : SeleniumTest
+    {
+        public override void Execute()
+        {
+            Open("http://froggeeframework.com");
+            Pause(1000);
+            Assert(Element(By.Id("converterBtn")).Click);
+            var inputElement = WaitUntil(Elements(By.TagName("input")).At(1));
+            Assert(Elements(By.TagName("input")).At(0).SendKeys(String.Format("This is {0} test", TestCase.Name)));
+            Assert(Element(inputElement).Clear.SendKeys("Hello!"));
+            Pause(2000);
+            Assert(Element(inputElement).Clear.SendKeys("Welcome to Froggee Framework!"));
+            Pause(2000);
+            Assert(Element(inputElement).Clear.SendKeys("We hope you will like it! :)"));
+            Pause(2000);
+            GoBack();
+            Pause(1000);
+        }
+    }
+}
